9b3c48eb39812ed9c4a82b4da2480455c4c3655c
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-26 18:30:27 +0200

    first ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,56 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
 
-const galleryContaner = document.querySelector('.gallery');
-const galaryCards = createGalaryCards(galleryItems);
-let modalImgBig;
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-galleryContaner.insertAdjacentHTML('beforeend', galaryCards);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCards = createGalleryCards(galleryItems);
 
-function createGalaryCards(galleryItems) {
+galleryContainer.insertAdjacentHTML('beforeend', galleryCards);
+
+function createGalleryCards(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-       <div class="gallery__item">
-         <a class="gallery__link" href="${original}">
-           <img
-             class="gallery__image"
-             src="${preview}"
-             data-source="${original}"
-             alt="${description}"/>
-         </a>
-       </div>
-       `;
+        <div class="gallery__item">
+          <a href="${original}" data-lightbox="image-1">
+            <img class="gallery__image" src="${preview}" alt="${description}" data-lightbox="image-1" />
+          </a>
+        </div>
+      `;
     })
     .join('');
 }
 
-galleryContaner.addEventListener('click', conteinerGalaryClick);
-
-function conteinerGalaryClick(evt) {
-  evt.preventDefault();
-
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-
-  console.log(evt.target.dataset.source);
-
-  modalImgBig = basicLightbox.create(`
-    <img src="${evt.target.dataset.source}" width="800" height="600">
-  `);
-
-  modalImgBig.show();
-}
-
-window.addEventListener('keydown', onEscKeyPress);
-function onEscKeyPress(evt) {
-  if (evt.code !== 'Escape') {
-    return;
-  }
-  {
-    modalImgBig.close();
-  }
-  window.removeEventListener('keydown', modalImgBig);
-}
+const gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
