9487a39ad5deaea3b4047a8414dad5c353d8ef68
Oleksii Yasynetskyi <ks7495395@gmail.com>
2023-02-28 15:53:33 +0200

    3 midle ok

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,77 @@
+// import throttle from 'lodash.throttle';
+
+// const refs = {
+//   form: document.querySelector('.feedback-form'),
+//   textarea: document.querySelector('.feedback-form  textarea'),
+// };
+
+// const STORAGE_KEY = 'feedback-form-state';
+
+// refs.form.addEventListener('submit', onFormSubmit);
+// refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+// onTextareaOutput();
+
+// function onFormSubmit(evt) {
+//   evt.preventDefault();
+
+//   console.log('Отправляем форму');
+//   evt.currentTarget.reset();
+//   localStorage.removeItem(STORAGE_KEY);
+// }
+
+// function onTextareaInput(evt) {
+//   const message = evt.target.value;
+//   localStorage.setItem(STORAGE_KEY, message);
+// }
+
+// function onTextareaOutput() {
+//   const saveMassage = localStorage.getItem(STORAGE_KEY);
+//   if (saveMassage) {
+//     console.log(saveMassage);
+//     refs.textarea.value = saveMassage;
+//   }
+// }
+
+//
+// refs.form.addEventListener('submit', onFormSubmit);
+
 import throttle from 'lodash.throttle';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form  textarea'),
+};
+const formData = {};
+const STORAGE_KEY = 'feedback-form-state';
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
+onTextareaOutput();
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  console.log('Отправляем форму');
+  evt.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onTextareaInput(evt) {
+  console.log(evt.target.name);
+  console.log(evt.target.value);
+
+  formData[evt.target.name] = evt.target.value;
+
+  console.log(formData);
+}
+//    const message = evt.target.email;
+//   localStorage.setItem(STORAGE_KEY, message);
+// }
+
+function onTextareaOutput() {
+  const saveMassage = localStorage.getItem(STORAGE_KEY);
+  if (saveMassage) {
+    console.log(saveMassage);
+    refs.textarea.value = saveMassage;
+  }
+}
